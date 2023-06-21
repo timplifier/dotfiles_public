ebe1e8e0d6654d01a75ee2b4b679a2c1232ca46b
timplifier <timplifier@gmail.com>
2023-06-21 20:22:05 +0600

    feat(nvim/lua/user): added user config

---
+++ .config/nvim/lua/user/plugins/community.lua
@@ -0,0 +1,18 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.docker" },
+}
